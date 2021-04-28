648e51532e6354344ad0418f46f4598c548cdf33
Spencer <sjones97@email.unc.edu>
2021-04-28 17:02:02 -0400

    add backend create function

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -3,3 +3,5 @@ import axios from 'axios'; // used to make api calls
 const url = 'http://localhost:5000/posts';
 
 export const fetchPosts = () => axios.get(url);
+
+export const createPost = (newPost) => axios.post(url, newPost);
--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -14,7 +14,7 @@ export const getPosts = async (req, res) => {
 };
 
 export const createPost = async (req, res) => {
-    const body = req.body;
+    const post = req.body;
 
     const newPost = new PostMessage(post);
 
