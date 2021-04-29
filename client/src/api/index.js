1b31decdf01a60a83500a99852224480e97aab15
Spencer <sjones97@email.unc.edu>
2021-04-29 13:06:45 -0400

    add sign in and sign up form

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'; // used to make api calls
 
-const url = 'https://mem-mern-app.herokuapp.com/posts';
+const url = 'http://localhost:5000/posts';
 
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
