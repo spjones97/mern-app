ff0a6c954eaa3a14cb5486fe68dc5da66a5e4990
Spencer <sjones97@email.unc.edu>
2021-04-29 10:04:46 -0400

    add like and delete feature

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -7,7 +7,7 @@ export const getPosts = () => async (dispatch) => {
 
         dispatch({ type: 'FETCH_ALL', payload: data });
     } catch (error) {
-        console.log(error.message);
+        console.log(error);
     }
 }
 
@@ -27,6 +27,26 @@ export const updatePost = (id, post) => async (dispatch) => {
 
         dispatch({ type: 'UPDATE', payload: data });
     } catch (error) {
-        console.log(error.message);
+        console.log(error);
+    }
+}
+
+export const deletePost = (id) => async (dispatch) => {
+    try {
+        await api.deletePost(id);
+
+        dispatch({ type: 'DELETE', payload: id });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const likePost = (id) => async (dispatch) => {
+    try {
+        const { data } = await api.likePost(id);
+
+        dispatch({ type: 'LIKE', payload: data });
+    } catch (error) {
+        console.log(error);
     }
 }
