408376539ebd06c22bd92278bf76c200d2742aee
Spencer <sjones97@email.unc.edu>
2021-04-29 11:16:31 -0400

    deploy application

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'; // used to make api calls
 
-const url = 'http://localhost:5000/posts';
+const url = 'https://mem-mern-app.herokuapp.com/posts';
 
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
